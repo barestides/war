574594ebeba75c2e4ca9044b04366a892b6f4a48
Braden Arestides <braden@arestides.com>
2016-06-28 12:52:10 -0400

    War games work correctly, histogram is output

--- doWar.cpp
+++ doWar.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 /*
   The file implements main to simulate 100000 games of war.  It should print out
@@ -12,14 +13,47 @@
 using namespace std;
 
 int main() {
-    // Fill in code here!
-    srand(time(0));
-    /*Deck d;
-    for (int i = 51; i >= 0; i--) {
-        cout << d.size() << "\t" << d.getTopCard() << endl;
+    srand(time(NULL));
 
-    }*/
-    runWar(1000);
+    //Map for storing round number distribution
+    map<int, int>histogram;
+
+    //how many games to run
+    int num_games = 1000;
+    int increment = num_games / 50;
+    for(int i = increment; i <= num_games; i = i + increment){
+        cout << i << endl;
+        histogram[i] = 0;
+    }
+
+    int total_rounds = 0;
+    for(int i = 0; i < num_games; i++){
+        int num_rounds = runWar(1000);
+        total_rounds = total_rounds + num_rounds;
+
+        map<int, int>::iterator it;
+        for(it = histogram.begin(); it != histogram.end(); it++){
+            if(num_rounds <= it->first){
+                it->second++;
+                break;
+            }
+        }
+    }
+
+    double average = total_rounds / num_games;
+
+    //output histo results
+    map<int, int>::iterator it;
+    for(it = histogram.begin(); it != histogram.end(); it++){
+        cout << it->first - increment + 1 << " - " << it->first <<  ":\t" << it->second << ":\t";
+        for(int i = 0; i < it->second; i++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "Average: " << average << endl;
+
     return 0;
 
 }
--- war.h
+++ war.h
@@ -17,15 +17,16 @@
 using namespace std;
 
 int runWar(unsigned int limit) {
+
     Deck full_deck;
     queue<Card> queue1, queue2;
 
     //split deck
-    for(int i = 0; i < 52; i++){
+    for (int i = 0; i < 52; i++) {
         Card card_from_full_deck = full_deck.getTopCard();
-        if(i%2 == 0){
+        if (i % 2 == 0) {
             queue1.push(card_from_full_deck);
-        }else{
+        } else {
             queue2.push(card_from_full_deck);
         }
     }
@@ -36,88 +37,96 @@ int runWar(unsigned int limit) {
     unsigned int comparison_counter = 0;
     while (comparison_counter < limit && deck1.size() > 0 && deck2.size() > 0) {
 
-        cout << "Deck1 size: " << deck1.size() << "\t Deck2 size: " << deck2.size() << "\n";
-        cout << "Round: " << comparison_counter << endl;
+        //// << "Deck1 size: " << deck1.size() << "\t Deck2 size: " << deck2.size() << "\n";
+        //// << "Round: " << comparison_counter << endl;
 
         //compare cards
         Card card1 = deck1.getTopCard();
         Card card2 = deck2.getTopCard();
 
-        cout << "Deck1 card: " << card1 << "\nDeck2 card: " << card2 << "\n";
+        //// << "Deck1 card: " << card1 << "\nDeck2 card: " << card2 << "\n";
 
         unsigned int rank1 = card1.getRank();
         unsigned int rank2 = card2.getRank();
         if (rank1 > rank2) {
-            cout << "Deck1 Wins!\n\n";
+            //// << "Deck1 Wins!\n\n";
             deck1.addCard(card1);
             deck1.addCard(card2);
         } else if (rank2 > rank1) {
-            cout << "Deck2 Wins!\n\n";
+            //// << "Deck2 Wins!\n\n";
             deck2.addCard(card1);
             deck2.addCard(card2);
         } else if (rank2 == rank1) {
-            cout << "War!\n\n";
+            // << "War!\n\n";
             vector<Card> cards_out;
             cards_out.push_back(card1);
             cards_out.push_back(card2);
 
             bool in_war = true;
-            while(in_war) {
-                Card war_card1, war_card2;
+            /* need 4 cards for a round of war; as per the rules, if a deck is empty at any time, that deck loses
+               because of this, each deck needs at least 5 cards to have an unempty deck after the war
+             */
 
-                //Need 4 cards total for one round of "war"
-                if(deck1.size() >= 4){
+            while (in_war) {
+                if (deck1.size() > 4 && deck2.size() > 4) {
                     for (int i = 0; i < 3; i++) {
                         cards_out.push_back(deck1.getTopCard());
-                    }
-                    war_card1 = deck1.getTopCard();
-                } else if(deck1.size() == 1){
-                    war_card1 = deck1.getTopCard();
-                }
-
-                if(deck2.size() >= 4){
-                    for (int i = 0; i < 3; i++) {
                         cards_out.push_back(deck2.getTopCard());
                     }
-                    war_card2 = deck2.getTopCard();
-                } else if(deck2.size() == 1){
-                    war_card2 = deck2.getTopCard();
-                }
 
-                cards_out.push_back(war_card1);
-                cards_out.push_back(war_card2);
+                    Card war_card1 = deck1.getTopCard();
+                    Card war_card2 = deck2.getTopCard();
 
-                cout << "Warcard1: " << war_card1 << "\nWarcard2: " << war_card2 << endl;
-                cout << "Cards on table: " << cards_out.size() << endl;
-                if(war_card1.getRank() > war_card2.getRank()){
-                    cout << "Deck1 Wins!\n";
-                    for(unsigned int i = 0; i < cards_out.size(); i++){
-                        deck1.addCard(cards_out[i]);
+                    cards_out.push_back(war_card1);
+                    cards_out.push_back(war_card2);
+
+                    //// << "Warcard1: " << war_card1 << "\nWarcard2: " << war_card2 << endl;
+                    // << "Cards on table: " << cards_out.size() << endl;
+                    comparison_counter++;
+                    if (war_card1.getRank() > war_card2.getRank()) {
+                        // << "Deck1 Wins!\n";
+                        for (unsigned int i = 0; i < cards_out.size(); i++) {
+                            deck1.addCard(cards_out[i]);
+                        }
+                        in_war = false;
+                    } else if (war_card2.getRank() > war_card1.getRank()) {
+                        // << "Deck2 Wins!\n";
+                        for (unsigned int i = 0; i < cards_out.size(); i++) {
+                            deck2.addCard(cards_out[i]);
+                        }
+                        in_war = false;
+                    }
+
+                } else if (deck1.size() <= 4) {
+                    //Deck1 would have emptied during the war, so we empty it
+                    for (unsigned int i = 0; i < deck1.size(); i++) {
+                        deck1.getTopCard();
+
                     }
                     in_war = false;
-                } else if (war_card2.getRank() > war_card1.getRank()){
-                    cout << "Deck2 Wins!\n";
-                    for(unsigned int i = 0; i < cards_out.size(); i++){
-                        deck2.addCard(cards_out[i]);
+                } else if (deck2.size() <= 4) {
+                    for (unsigned int i = 0; i < deck2.size(); i++) {
+                        deck2.getTopCard();
                     }
                     in_war = false;
-                } else if (war_card1.getRank() == war_card2.getRank()){
-                    //war again
                 }
+
             }
+
         }
         comparison_counter++;
     }
 
-    if(deck1.size() == 0){
-        cout << "Deck2 Wins the Game!" << endl;
-    } else if (deck2.size() == 0){
-        cout << "Deck1 Wins the Game!" << endl;
+    if (deck1.size() == 0) {
+        // << "Deck2 Wins the Game!" << endl;
+    } else if (deck2.size() == 0) {
+        // << "Deck1 Wins the Game!" << endl;
     } else {
-        cout << "Draw!" << endl;
+        // << "Draw!" << endl;
     }
 
-    return 0;
+
+    return comparison_counter;
 }
 
 #endif
